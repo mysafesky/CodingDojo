c458a79379dde310603172056f72609c9818b117
Zhengcheng Gu <mysafesky@gmail.com>
2015-08-27 07:24:27 -0700

    mysql homeworks

---
+++ MySQL/Assignments/Assign4/assign4_query.sql
@@ -0,0 +1,7 @@
+SELECT users.id,users.first_name, users.last_name,user2.first_name as friend_first_name, user2.last_name as friend_last_name
+FROM users 
+LEFT JOIN friendships 
+ON users.id=friendships.user_id 
+LEFT JOIN users as user2 
+ON friendships.friend_id = user2.id
+ORDER by users.first_name ASC, user2.last_name ASC;
